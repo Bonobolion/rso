7c3a632fee2d6909857a0463d1f9c0c1719b89cc
Jose <jarirez@hotmail.com>
2019-10-21 17:05:24 -0500

    added secondary log in activity

--- app/src/main/java/com/example/myapplication/Login.kt
+++ app/src/main/java/com/example/myapplication/Login.kt
@@ -3,16 +3,17 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_initialization.*
 
 class Login : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_initialization)
 
         //this function initiates an intent object listening for a click on the logInButton,
         //once the button is pressed the login process begins
+
         logInButton.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
